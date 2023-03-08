f86a248002dc60f1a86260ada8507f075a238a03
Lighuen Restanio <lighuen.restanio@gmail.com>
2023-03-08 01:35:18 +0100

    Text field and card with information added

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,28 @@
       <h1>Crypto search</h1>
     </v-app-bar>
     <v-main>
-    
+      <v-container>
+        <v-card class="mx-auto" max-width="400">
+          <v-text-field label="Ingrese nombre de cryptomoneda" >
+          </v-text-field>
+          <v-btn>
+            Enviar
+          </v-btn>
+        </v-card>
+      </v-container>
+
+      <v-container>
+        <v-card class="mx-auto" max-width="400" prepend-icon="mdi-home">
+          <template v-slot:title>
+            This is a title
+          </template>
+          <v-card-text>
+            This is content
+          </v-card-text>
+        </v-card>
+      </v-container>
+
+
     </v-main>
   </v-app>
 </template>
@@ -16,7 +37,7 @@ export default {
   name: 'App',
 
   components: {
-    
+
   },
 
   data: () => ({
@@ -24,3 +45,10 @@ export default {
   }),
 }
 </script>
+
+<style>
+. {
+  padding: 0;
+  margin: ;
+}
+</style>
